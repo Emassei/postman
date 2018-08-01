a2f88eab01bed90b86e4167668fc5cc2e00edf86
Ernie Massei <ernie@crowdstreet.com>
2018-07-31 21:11:39 -0700

    Initial commit

---
+++ api.js
@@ -0,0 +1,22 @@
+var request = require('request');
+require('dotenv').config();
+
+var options = {
+  url: 'https://api.getpostman.com/collections',
+  headers: {
+    'X-Api-Key': process.env.API_KEY
+  }
+};
+ 
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    length = Object.keys(info.collections).length;
+    for (var i = 0; i < length; i++) {
+      console.log(info.collections[i].id + ' - ' +
+        info.collections[i].name);
+    }
+  }
+}
+ 
+request(options, callback);
